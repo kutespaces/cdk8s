96f9cf008e3ad68ad5d5152073218bcb16da204c
Felix Seidel <felix@seidel.me>
2023-03-01 03:47:57 +0000

    🐛 Fix path to register.mjs

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this runs at Codespace creation - not part of pre-build
-node /workspaces/cdk8s/.kutespaces/cli/register.js
+node /workspaces/cdk8s/.kutespaces/cli/register.mjs
 node /workspaces/cdk8s/.kutespaces/cli/event.mjs --name space:create
 
 node /workspaces/cdk8s/.kutespaces/cli/event.mjs --name space:create-done
